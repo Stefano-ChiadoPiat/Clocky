3e304acb4a27cdb00a0281daeb6f4f6e4c561f83
Stefano Chiadò Piat <chiadopiat.stefano@itisgrassi.edu.it>
2025-05-08 18:16:27 +0200

    Fix

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function updateClock() 
 {
-    const tomorrow = new Date(); // Create a new Date object representing the current date and time
-    const hours = String(tomorrow.getHours()).padStart(2, '0'); // Get the current hours and convert it to a string, replacing it with a 2-digit format (e.g., "01", "02", ..., "23")
-    const minutes = String(tomorrow.getMinutes()).padStart(2, '0'); // Get the current minutes and convert it to a string, replacing it with a 2-digit format (e.g., "00", "01", ..., "59")
-    const seconds = String(tomorrow.getSeconds()).padStart(2, '0'); // Get the current seconds and convert it to a string, replacing it with a 2-digit format (e.g., "00", "01", ..., "59")
+    const now = new Date(); // Create a new Date object representing the current date and time
+    const hours = String(now.getHours()).padStart(2, '0'); // Get the current hours and convert it to a string, replacing it with a 2-digit format (e.g., "01", "02", ..., "23")
+    const minutes = String(now.getMinutes()).padStart(2, '0'); // Get the current minutes and convert it to a string, replacing it with a 2-digit format (e.g., "00", "01", ..., "59")
+    const seconds = String(now.getSeconds()).padStart(2, '0'); // Get the current seconds and convert it to a string, replacing it with a 2-digit format (e.g., "00", "01", ..., "59")
     document.getElementById('display').textContent = `${hours}:${minutes}:${seconds}`; // Update the content of the element with id "display" with the current time
 }
 
